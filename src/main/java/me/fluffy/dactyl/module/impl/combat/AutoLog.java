88090a8d4fb2c2d4900982e863fab1b835436fb5
fluffy <fluffycqgithub@gmail.com>
2020-11-21 18:04:44 -0500

    Delete AutoLog.java

--- src/main/java/me/fluffy/dactyl/module/impl/combat/AutoLog.java
+++
@@ -1,9 +0,0 @@
-package me.fluffy.dactyl.module.impl.combat;
-
-import me.fluffy.dactyl.module.Module;
-
-public class AutoLog extends Module {
-    public AutoLog() {
-        super("AutoLog", Category.COMBAT);
-    }
-}
