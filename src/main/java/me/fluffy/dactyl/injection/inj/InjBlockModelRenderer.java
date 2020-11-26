8f0e712f9a1a785a5bfa4d09d18e5a73620087c6
fluffy <fluffycqgithub@gmail.com>
2020-11-25 22:58:22 -0500

    Update InjBlockModelRenderer.java

--- src/main/java/me/fluffy/dactyl/injection/inj/InjBlockModelRenderer.java
+++ src/main/java/me/fluffy/dactyl/injection/inj/InjBlockModelRenderer.java
@@ -1,11 +1,9 @@
 package me.fluffy.dactyl.injection.inj;
 
 import me.fluffy.dactyl.module.impl.render.XRay;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.BlockModelRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
