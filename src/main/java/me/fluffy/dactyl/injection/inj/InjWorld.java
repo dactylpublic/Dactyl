1f137251232bf904275079e6a51e694c7b18b228
fluffy <fluffycqgithub@gmail.com>
2020-11-25 22:24:41 -0500

    norender skylight

--- src/main/java/me/fluffy/dactyl/injection/inj/InjWorld.java
+++ src/main/java/me/fluffy/dactyl/injection/inj/InjWorld.java
@@ -1,13 +1,18 @@
 package me.fluffy.dactyl.injection.inj;
 
 import me.fluffy.dactyl.event.impl.world.EntityRemovedEvent;
+import me.fluffy.dactyl.module.impl.render.NoRender;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(World.class)
 public class InjWorld {
@@ -15,5 +20,12 @@ public class InjWorld {
     @Inject(method = "onEntityRemoved", at = @At("HEAD"), cancellable = true)
     public void onEntityRemoved(Entity entity, CallbackInfo callbackInfo) {
         MinecraftForge.EVENT_BUS.post(new EntityRemovedEvent(entity));
+    }
+
+    @Inject(method = {"checkLightFor"}, at = {@At("HEAD")}, cancellable = true)
+    private void checkLightFor(EnumSkyBlock enumSkyBlock, BlockPos blockPos, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        if(NoRender.INSTANCE.isEnabled()) {
+            callbackInfoReturnable.setReturnValue(Boolean.valueOf(false));
+        }
     }
 }
