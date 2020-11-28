c7974187eceee48a5ee8a7638c00af7f72b06612
fluffy <fluffycqgithub@gmail.com>
2020-11-28 01:24:38 -0500

    mountbypass

--- src/main/java/me/fluffy/dactyl/module/impl/movement/EntityControl.java
+++ src/main/java/me/fluffy/dactyl/module/impl/movement/EntityControl.java
@@ -1,11 +1,31 @@
 package me.fluffy.dactyl.module.impl.movement;
 
+import me.fluffy.dactyl.event.impl.network.PacketEvent;
 import me.fluffy.dactyl.module.Module;
+import me.fluffy.dactyl.setting.Setting;
+import net.minecraft.entity.passive.AbstractChestHorse;
+import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EntityControl extends Module {
+    public Setting<Boolean> mountBypass = new Setting<Boolean>("MountBypass", true);
     public static EntityControl INSTANCE;
     public EntityControl() {
         super("EntityControl", Category.MOVEMENT);
         INSTANCE = this;
+    }
+
+    @SubscribeEvent
+    public void onPacket(PacketEvent event) {
+        if(mc.player == null || mc.world == null) {
+            return;
+        }
+        if(mountBypass.getValue()) {
+            if(event.getPacket() instanceof CPacketUseEntity) {
+                if (((CPacketUseEntity)event.getPacket()).getEntityFromWorld(mc.world) instanceof AbstractChestHorse && ((CPacketUseEntity)event.getPacket()).getAction() == CPacketUseEntity.Action.INTERACT_AT) {
+                    event.setCanceled(true);
+                }
+            }
+        }
     }
 }
