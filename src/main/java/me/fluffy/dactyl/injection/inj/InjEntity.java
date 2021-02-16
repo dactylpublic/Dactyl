8f10459fdd74560e00087ceb75d87f8b0fda7325
fluffy <fluffycqgithub@gmail.com>
2021-02-16 02:05:57 -0500

    scaffold sakewalk

--- src/main/java/me/fluffy/dactyl/injection/inj/InjEntity.java
+++ src/main/java/me/fluffy/dactyl/injection/inj/InjEntity.java
@@ -1,11 +1,14 @@
 package me.fluffy.dactyl.injection.inj;
 
+import me.fluffy.dactyl.module.impl.player.Scaffold;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MoverType;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Entity.class)
 public abstract class InjEntity{
@@ -36,4 +39,9 @@ public abstract class InjEntity{
 
     @Shadow
     public double motionZ;
+
+    @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSneaking()Z"))
+    public boolean isSneaking(Entity entity) {
+        return Scaffold.INSTANCE.isEnabled() || entity.isSneaking();
+    }
 }
