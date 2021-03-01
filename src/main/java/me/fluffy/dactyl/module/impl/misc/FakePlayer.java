aec8fbd99aa1daa647a0ecd2efe4570d6c8f16ad
fIuffy <fluffycqgithub@gmail.com>
2021-03-01 15:41:31 -0500

    Update FakePlayer.java

--- src/main/java/me/fluffy/dactyl/module/impl/misc/FakePlayer.java
+++ src/main/java/me/fluffy/dactyl/module/impl/misc/FakePlayer.java
@@ -6,14 +6,17 @@ import me.fluffy.dactyl.setting.Setting;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class FakePlayer extends Module {
-    Setting<String> username = new Setting<String>("Name", "Boner");
+    Setting<String> username = new Setting<String>("Name", "Catgirl");
+    Setting<Boolean> copyPotions = new Setting<Boolean>("CopyPotions", true);
     public FakePlayer() {
         super("FakePlayer", Category.MISC);
     }
@@ -27,6 +30,11 @@ public class FakePlayer extends Module {
         EntityOtherPlayerMP fake = new EntityOtherPlayerMP((World)mc.world, new GameProfile(UUID.fromString("069a79f4-44e9-4726-a5be-fca90e38aaf5"), username.getValue()));
         fake.copyLocationAndAnglesFrom(mc.player);
         fake.inventory.copyInventory(mc.player.inventory);
+        if(copyPotions.getValue()) {
+            for (Map.Entry<Potion,PotionEffect> entry : mc.player.activePotionsMap.entrySet()) {
+                fake.addPotionEffect(entry.getValue());
+            }
+        }
         mc.world.addEntityToWorld(-4201337, (Entity)fake);
     }
 
