886003934ed83224dccf809648627eebdeb5d0a2
fIuffy <fluffycqgithub@gmail.com>
2021-03-30 13:32:07 -0400

    Update CombatUtil.java

--- src/main/java/me/fluffy/dactyl/util/CombatUtil.java
+++ src/main/java/me/fluffy/dactyl/util/CombatUtil.java
@@ -1927,37 +1927,56 @@ public class CombatUtil {
     }
 
     public static float calculateDamage(final double posX, final double posY, final double posZ, final Entity entity) {
-        final float doubleExplosionSize = 12.0f;
-        final double distancedsize = entity.getDistance(posX, posY, posZ) / doubleExplosionSize;
-        final Vec3d vec3d = new Vec3d(posX, posY, posZ);
-        final double blockDensity = entity.world.getBlockDensity(vec3d, entity.getEntityBoundingBox());
-        final double v = (1.0 - distancedsize) * blockDensity;
-        final float damage = (float)(int)((v * v + v) / 2.0 * 7.0 * doubleExplosionSize + 1.0);
-        double finald = 1.0;
-        if (entity instanceof EntityLivingBase) {
-            finald = getBlastReduction((EntityLivingBase)entity, getDamageMultiplied(damage), new Explosion((World)mc.world, (Entity)null, posX, posY, posZ, 6.0f, false, true));
+        if(entity == null) {
+            return 0.0f;
         }
-        return (float)finald;
+        try {
+            final float doubleExplosionSize = 12.0f;
+            final double distancedsize = entity.getDistance(posX, posY, posZ) / doubleExplosionSize;
+            final Vec3d vec3d = new Vec3d(posX, posY, posZ);
+            final double blockDensity = entity.world.getBlockDensity(vec3d, entity.getEntityBoundingBox());
+            final double v = (1.0 - distancedsize) * blockDensity;
+            final float damage = (float) (int) ((v * v + v) / 2.0 * 7.0 * doubleExplosionSize + 1.0);
+            double finald = 1.0;
+            if (entity instanceof EntityLivingBase) {
+                finald = getBlastReduction((EntityLivingBase) entity, getDamageMultiplied(damage), new Explosion((World) mc.world, (Entity) null, posX, posY, posZ, 6.0f, false, true));
+            }
+            return (float) finald;
+        } catch(Exception e) {
+            //null
+            return 0.0f;
+        }
     }
 
     public static float getBlastReduction(final EntityLivingBase entity, float damage, final Explosion explosion) {
-        if (entity instanceof EntityPlayer) {
-            final EntityPlayer ep = (EntityPlayer)entity;
-            final DamageSource ds = DamageSource.causeExplosionDamage(explosion);
-            damage = CombatRules.getDamageAfterAbsorb(damage, (float)ep.getTotalArmorValue(), (float)ep.getEntityAttribute(SharedMonsterAttributes.ARMOR_TOUGHNESS).getAttributeValue());
-            if(ep.getArmorInventoryList() == null || ds == null) {
+        try {
+            if (entity == null || explosion == null) {
                 return 0.0f;
             }
-            final int k = EnchantmentHelper.getEnchantmentModifierDamage(ep.getArmorInventoryList(), ds);
-            final float f = MathHelper.clamp((float)k, 0.0f, 20.0f);
-            damage *= 1.0f - f / 25.0f;
-            if (entity.isPotionActive(Potion.getPotionById(11))) {
-                damage -= damage / 4.0f;
+            if (entity instanceof EntityPlayer) {
+                final EntityPlayer ep = (EntityPlayer) entity;
+                final DamageSource ds = DamageSource.causeExplosionDamage(explosion);
+                if (ds == null) {
+                    return 0.0f;
+                }
+                damage = CombatRules.getDamageAfterAbsorb(damage, (float) ep.getTotalArmorValue(), (float) ep.getEntityAttribute(SharedMonsterAttributes.ARMOR_TOUGHNESS).getAttributeValue());
+                if (ep.getArmorInventoryList() == null || ds == null) {
+                    return 0.0f;
+                }
+                final int k = EnchantmentHelper.getEnchantmentModifierDamage(ep.getArmorInventoryList(), ds);
+                final float f = MathHelper.clamp((float) k, 0.0f, 20.0f);
+                damage *= 1.0f - f / 25.0f;
+                if (entity.isPotionActive(Potion.getPotionById(11))) {
+                    damage -= damage / 4.0f;
+                }
+                return damage;
             }
+            damage = CombatRules.getDamageAfterAbsorb(damage, (float) entity.getTotalArmorValue(), (float) entity.getEntityAttribute(SharedMonsterAttributes.ARMOR_TOUGHNESS).getAttributeValue());
             return damage;
+        } catch(Exception e) {
+            // null
+            return 0.0f;
         }
-        damage = CombatRules.getDamageAfterAbsorb(damage, (float)entity.getTotalArmorValue(), (float)entity.getEntityAttribute(SharedMonsterAttributes.ARMOR_TOUGHNESS).getAttributeValue());
-        return damage;
     }
 
     private static float getDamageMultiplied(final float damage) {
